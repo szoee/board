b73514bd89317d24971f95c697173bda7bd530c4
szoee <jysepijj@naver.com>
2024-06-17 11:49:23 +0900

    #18 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드로 테스트 통과하게경로 잡아줌
    대략적으로 뷰 index와 header, footer 뼈대 잡음
    테스트 메소드 작성

--- src/main/java/com/bitstudy/board/controller/ArticleController.java
+++ src/main/java/com/bitstudy/board/controller/ArticleController.java
@@ -1,12 +1,6 @@
 package com.bitstudy.board.controller;
 
-/*
-뷰 엔드 포인트
- */
-
-import com.bitstudy.board.domain.Article;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +13,10 @@ public class ArticleController {
 
     @GetMapping
     public String article(ModelMap model) {
-        /* Model은 인터페이스 */
+        /* Model은 인터페이스
+            ModelMap은 클래스(구현체) 사실 둘이 다른건 없음. Model 써도 같은 결과임   */
 
         model.addAttribute("articles", List.of());
-        return "articles/index";
+        return "/articles/Ex07_1_index";
     }
 }
--- src/main/java/com/bitstudy/board/controller/Ex06_1_ArticleController.java
+++ src/main/java/com/bitstudy/board/controller/Ex06_1_ArticleController.java
@@ -1,9 +1,5 @@
 package com.bitstudy.board.controller;
 
-/*
-뷰 엔드 포인트
- */
-
 import com.bitstudy.board.domain.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,13 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@Controller
+//@Controller
 @RequestMapping("/articles")
 public class Ex06_1_ArticleController {
 
     @GetMapping
     public String article(ModelMap model) {
-        /* Model은 인터페이스 */
+        /* Model은 인터페이스
+            ModelMap은 클래스(구현체) 사실 둘이 다른건 없음. Model 써도 같은 결과임   */
 
         model.addAttribute("articles", List.of());
         return "articles/index";
